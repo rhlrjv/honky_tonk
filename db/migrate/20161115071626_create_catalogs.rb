0c4f945479a13f6642075610d85f56a1a55749df
Rahul Rajeev <rahulrajeev91@gmail.com>
2016-11-15 15:21:26 +0800

    Generate scaffold for catalogs

---
+++ db/migrate/20161115071626_create_catalogs.rb
@@ -0,0 +1,11 @@
+class CreateCatalogs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :catalogs do |t|
+      t.string :sku
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110085206) do
+ActiveRecord::Schema.define(version: 20161115071626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "catalogs", force: :cascade do |t|
+    t.string   "sku"
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
